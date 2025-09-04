45b04f321f0be88b7ffe8fd8ad8b6ce431f61f8b
Komal Dumbare <komaldumbare427@gmail.com>
2025-09-04 15:09:07 +0530

    Basic Problem

---
+++ Greater.java
@@ -0,0 +1,13 @@
+class Greater{
+    public static void main(String[] args)
+    {
+        int a = 10, b= 15;
+        if(a > b)
+        {
+            System.out.println("The a is greater");
+        }
+        else{
+            System.out.println("The b is greater");
+        }
+    }
+}
