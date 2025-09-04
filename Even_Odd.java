7afc66a55c846918b849e2529dee8bb7a97637c9
Komal Dumbare <komaldumbare427@gmail.com>
2025-09-04 15:15:38 +0530

    Basic if-else

---
+++ Even_Odd.java
@@ -0,0 +1,13 @@
+class Even_Odd{
+    public static void main(String[] args)
+    {
+        int n = 5;
+        if(n % 2 == 0)
+        {
+            System.out.println("The number is even");
+        }
+        else{
+            System.out.println("The number is odd");
+        }
+    }
+}
