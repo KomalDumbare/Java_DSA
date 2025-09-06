73ac3ec68cfe4dd57cc40602389250b01f91fdbb
Komal Dumbare <komaldumbare427@gmail.com>
2025-09-06 18:42:11 +0530

    Patterns in java

--- LeadersO.java
+++ LeadersO.java
@@ -3,7 +3,7 @@ class LeadersO {
         public void getResult(int arr[],int n)
         {
             ArrayList<Integer> LeaderList = new ArrayList<>();
-            int max = Integer.MIN_VALUE;;
+            int max = Integer.MIN_VALUE;
             for(int i=n-1;i>=0;i--)
             {
                 if(arr[i] > max)
