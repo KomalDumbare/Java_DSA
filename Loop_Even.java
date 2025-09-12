1d89f8645c65e54d2651e55d3b4595bdac5a67f0
Komal Dumbare <komaldumbare427@gmail.com>
2025-09-12 16:35:21 +0530

    While-loop

---
+++ Loop_Even.java
@@ -0,0 +1,18 @@
+public class Loop_Even {
+    public static void main(String[] args)
+    {
+        int i = 1;
+        while(i <= 10)
+        {
+            if(i % 2 == 0)
+            {
+                System.out.print(i+" ");
+            }
+            i++;
+        }
+    }
+}
+
+Output: 
+
+2 4 6 8 10 
