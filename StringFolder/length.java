d9f0a293695abd59c366fe3d9517275d2d9d86cb
Komal Dumbare <komaldumbare427@gmail.com>
2025-09-21 06:35:12 +0530

    length() method

---
+++ StringFolder/length.java
@@ -0,0 +1,11 @@
+package StringFolder;
+
+public class length {
+    public static void main(String[] args)
+    {
+        String s = "Hello";
+        int var = s.length();
+        System.out.println(var);
+    }
+    
+}
