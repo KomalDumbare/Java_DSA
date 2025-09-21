8378d694bb9a2e5e38ea9df9cd6d271ae064b20e
Komal Dumbare <komaldumbare427@gmail.com>
2025-09-21 07:01:53 +0530

    toLowerCase() and toUpperCase() method

---
+++ StringFolder/LowerUpperMethod.java
@@ -0,0 +1,19 @@
+package StringFolder;
+
+public class LowerUpperMethod {
+    public static void main(String[] args)
+    {
+        // lower case
+        String s1 = "HELLO";
+        System.out.println("Original String: "+s1);
+        String ans = s1.toLowerCase();
+        System.out.println("Lowercase String: "+ans);
+
+        //lowercase
+        String s2 = "hello";
+        System.out.println("Original String: "+s2);
+        String an = s2.toUpperCase();
+        System.out.println("Uppercase String: "+an);
+    }
+    
+}
